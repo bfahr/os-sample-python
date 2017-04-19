7318cf85802ebbafcc7fd08a21f8ae6cd80b7af3
Bob Fahr <bfahr@redhat.com>
2017-04-19 12:59:23 -0500

    Added new url

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,19 @@
+import time
 from flask import Flask
+
 application = Flask(__name__)
+
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+
+@application.route("/algo")
+def algo():
+    time.sleep(10)
+    return "The answer is 2"
+
 
 if __name__ == "__main__":
     application.run()
