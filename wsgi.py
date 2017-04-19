cf2e3ca232a5b51da16663ae08cb06a486952de2
Bob Fahr <bfahr@redhat.com>
2017-04-19 13:18:37 -0500

    Added custom class and json response

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,42 @@
 import time
-from flask import Flask
+from flask import Flask, json, Response
 
-application = Flask(__name__)
+
+class ApiResult(object):
+
+    def __init__(self, value, status=200):
+        self.value = value
+        self.status = status
+
+    def to_response(self):
+        return Response(json.dumps(self.value),
+                        status=self.status,
+                        mimetype='application/json',
+                        headers={'Access-Control-Allow-Origin': '*'})
+
+
+class ApiException(Exception):
+
+    def __init__(self, message, status=400):
+        self.message = message
+        self.status = status
+
+    def to_result(self):
+        return ApiResult({'message': self.message},
+                         status=self.status)
+
+
+class ApiFlask(Flask):
+
+    def make_response(self, rv):
+        if isinstance(rv, ApiResult):
+            return rv.to_response()
+        return Flask.make_response(self, rv)
+
+
+application = ApiFlask(__name__)
+application.register_error_handler(
+    ApiException, lambda err: err.to_result())
 
 
 @application.route("/")
@@ -12,7 +47,13 @@ def hello():
 @application.route("/algo")
 def algo():
     time.sleep(10)
-    return "The answer is 2"
+    results = {
+        'name': 'algorithm 3',
+        'input': '1.5',
+        'output': 2,
+        'duration': 10
+    }
+    return ApiResult(results)
 
 
 if __name__ == "__main__":
